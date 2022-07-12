b016bd536b08ecef0f020555361e3f91b3d5233f
pedro freire <pedrofc31.31@gmail.com>
2022-07-12 20:33:56 -0300

    adjusting css

--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -4,12 +4,14 @@ const styles = StyleSheet.create({
     formContext: {
         width: '100%',
         height: '100%',
+        justifyContent: 'space-around',
         bottom: 0,
         backgroundColor: '#ffffff',
         alignItems: 'center',
         borderTopLeftRadius: 30,
         borderTopRightRadius: 30,
-        marginTop: 30,
+        paddingTop: 15,
+        marginTop: 20,
     },
     form: {
         width: '100%',
